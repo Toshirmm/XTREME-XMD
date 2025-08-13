dbc451b9052463e8f6fbc0e524068a23f3cd5084
Toshirmm <144406472+Toshirmm@users.noreply.github.com>
2025-08-13 19:53:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~DgxDBSQS#vvpWheI2Ztoe1unx2zDMGUmvKmD8L8xyjaKw6RSgaOQ",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~X8YWUSrB#7zR2_oZEvPxfDwmlo8lWgc1eAac-g4C6WIWX4R27ukg",
 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/e6orj5.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*ʜɪ ᴅᴇᴀʀ👋 ɪᴍ xᴛʀᴇᴍᴇ xᴍᴅ ᴄʜᴜᴅᴅʏ ʙᴏᴜᴅʏ🦋 ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ғᴏʀ ᴀsɪsᴛ ʏᴏᴜ.*",
@@ -27,6 +27,6 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "shirone arian",
+BOT_NAME: process.env.BOT_NAME || "XTREME-XMD",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
