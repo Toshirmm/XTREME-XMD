64ec74ed348679e4b4994f1a031b47e8015e7cad
Toshirmm <144406472+Toshirmm@users.noreply.github.com>
2025-09-05 21:02:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~D8xVQZCb#x9YB_s2PgF9XifbQapCQ4xFWi90xh01eSnuL1cHAzY0",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~Xsx0hAJR#d1ZNMEyp3ZjA3McWMzoaoYs0wvgSzDXftzk0YIGLH28",
 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
@@ -27,6 +27,6 @@ CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "XTREME-XMD",
+BOT_NAME: process.env.BOT_NAME || "shirone arian",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
